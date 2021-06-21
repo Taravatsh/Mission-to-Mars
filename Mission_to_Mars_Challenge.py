3654322a4071baee1fd2418a0c045271086241b1
Taravat <eau0715362@gmail.com>
2021-06-20 21:43:04 -0700

    Python file exported.

--- Mission_to_Mars_Challenge.py
+++ Mission_to_Mars_Challenge.py
@@ -1,28 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-# In[71]:
-
-
 # Set the executable path and initialize Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
-
-
-# ### Visit the NASA Mars News Site
-
-# In[ ]:
-
 
 # Visit the mars nasa news site
 url = 'https://redplanetscience.com/'
@@ -31,111 +15,56 @@ browser.visit(url)
 # Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-
-# In[ ]:
-
-
 # Convert the browser html to a soup object and then quit the browser
 html = browser.html
 news_soup = soup(html, 'html.parser')
 
 slide_elem = news_soup.select_one('div.list_text')
 
-
-# In[ ]:
-
-
 slide_elem.find('div', class_='content_title')
-
-
-# In[ ]:
-
 
 # Use the parent element to find the first a tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
 
-
-# In[ ]:
-
-
 # Use the parent element to find the paragraph text
 news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 news_p
-
-
-# ### JPL Space Images Featured Image
-
-# In[ ]:
-
 
 # Visit URL
 url = 'https://spaceimages-mars.com'
 browser.visit(url)
 
-
-# In[ ]:
-
-
 # Find and click the full image button
 full_image_elem = browser.find_by_tag('button')[1]
 full_image_elem.click()
-
-
-# In[ ]:
-
 
 # Parse the resulting html with soup
 html = browser.html
 img_soup = soup(html, 'html.parser')
 img_soup
 
-
-# In[ ]:
-
-
 # find the relative image url
 img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 img_url_rel
-
-
-# In[ ]:
-
 
 # Use the base url to create an absolute url
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
 
-
-# ### Mars Facts
-
-# In[ ]:
-
+# Mars Facts
 
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
 df.head()
-
-
-# In[ ]:
-
 
 df.columns=['Description', 'Mars', 'Earth']
 df.set_index('Description', inplace=True)
 df
 
-
-# In[ ]:
-
-
 df.to_html()
 
 
-# # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
-
-# ### Hemispheres
-
-# In[72]:
-
+## D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
 
 # 1. Use browser to visit the URL 
 url = 'https://marshemispheres.com/'
@@ -144,62 +73,34 @@ browser.visit(url)
 html = browser.html
 img_url_title_soup = soup(html, 'html.parser')
 
-
-# In[73]:
-
-
 # 2. Create a list to hold the images and titles.
 hemisphere_image_urls = []
 
 # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+# Create a for loop to iterate through the CSS elements for all the four images and titles.
 for i in range(0,4):
+    
+    # Create an empty dictionary.
     hemisphere = {}
+    # Click on the each hemisphere link.
     all_links = browser.find_by_css('a.itemLink h3')[i].click()
+    # Navigate to the full resolution image URL by finding the "Sample" image anchor tag.
     full_image_page = browser.find_by_text('Sample')
+    # Retrieve the href of the image.
     full_image_url = full_image_page['href']
+    # Retrieve the title for the hemisphere image.
     image_title = browser.find_by_css('h2.title').text
-    hemisphere = {'img_url':full_image_url, 'title':image_title}
+    # Save the image url string and image title as values for img_url and title keys.
+    hemisphere = {'img_url': full_image_url, 'title': image_title}
+    # Add the dictionary to the list.
     hemisphere_image_urls.append(hemisphere)
+    # Navigate back to the beginning to retrieve data of the next image.
     browser.back()
-
-
-# In[74]:
-
 
 # 4. Print the list that holds the dictionary of each image url and title.
 hemisphere_image_urls
 
-
-# In[66]:
-
-
-all_links = browser.find_by_css('a.product-item h3')[2].click()
-
-
-# In[67]:
-
-
-full_image_page = browser.find_by_text('Sample')[0]
-full_image_url = full_image_page['href']
-image_title = browser.find_by_css('h2.title').text
-image_title
-
-
-# In[68]:
-
-
 # 5. Quit the browser
 browser.quit()
-
-
-# In[54]:
-
-
-all_links = browser.find_by_css('a.itemLink h3')[0].click()
-
-
-# In[55]:
-
-
-full_image_page = browser.find_by_text('Sample')[0].click()
 
